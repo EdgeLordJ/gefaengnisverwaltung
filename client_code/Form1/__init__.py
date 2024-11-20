220cff84fea3a5b2a2f8d811d4d0320411b15e9a
joudi.khalil@student.htl-rankweil.at <joudi.khalil@student.htl-rankweil.at>
2024-11-20 11:30:30 +0000

    Edited backend and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -13,14 +13,17 @@ class Form1(Form1Template):
     
     # Any code you write here will run before the form opens.
     self.gefaengnisse_drop_down.items = anvil.server.call('get_gefaengnisse')
-    self.label_direktor.text = "Direktor TODO" 
-    self.label_freie_zellen.text = "Freie Zellen TODO"
+    vid = self.gefaengnisse_drop_down.items[self.gefaengnisse_drop_down.selected_value-1][1]
+    self.label_direktor.text = f"Direktor {anvil.server.call('get_direktor', vid)[0][0]}" 
+    self.label_freie_zellen.text = f"Freie Zellen {anvil.server.call('get_direktor', vid)[0][1]}"
     self.repeating_zellen.items = [{'zellennummer': 'TODO', 'anzahl_häftlinge': 'TODO'}, 
                                    {'zellennummer': 'TODO', 'anzahl_häftlinge': 'TODO'}]
 
   def gefaengnisse_drop_down_change(self, **event_args):
     """This method is called when an item is selected"""
-    pass
+    vid = self.gefaengnisse_drop_down.items[self.gefaengnisse_drop_down.selected_value-1][1]
+    self.label_direktor.text = f"Direktor {anvil.server.call('get_direktor', vid)[0][0]}" 
+    self.label_freie_zellen.text = f"Freie Zellen {anvil.server.call('get_direktor', vid)[0][1]}"
 
  
 
--- server_code/backend.py
+++ server_code/backend.py
@@ -8,4 +8,23 @@ import sqlite3
 
 @anvil.server.callable
 def get_gefaengnisse():
-  return [('TODO 1', 1), ('TODO 2', 2)]
+  conn = sqlite3.connect(data_files['prison_management.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute('SELECT Name, VID FROM Gefaengnis'))
+  return res
+
+@anvil.server.callable
+def get_direktor(vid):
+  conn = sqlite3.connect(data_files['prison_management.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(f'SELECT Direktor, Anzahl_freie_Zellen FROM Verwaltung WHERE VID = {vid}'))
+  print(vid, res)
+  return res
+
+@anvil.server.callable
+def get_zelle(gid):
+  conn = sqlite3.connect(data_files['prison_management.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(f'SELECT Zellennummer FROM Zelle WHERE Name = {gid}'))
+  print(gid, res)
+  return res
